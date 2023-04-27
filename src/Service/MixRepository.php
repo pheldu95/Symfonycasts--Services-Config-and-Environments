e0dd520add6ec613bedca4a45c7d242686a0c4f9
pheldu95 <phelps.duncan@gmail.com>
2023-04-27 16:49:42 -0500

    used dependency injection so that our MixRepository service has access to the other services it need

--- src/Service/MixRepository.php
+++ src/Service/MixRepository.php
@@ -3,16 +3,25 @@
 namespace App\Service;
 
 use Psr\Cache\CacheItemInterface;
+use Symfony\Contracts\Cache\CacheInterface;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class MixRepository
 {
+    public function __construct(
+        private HttpClientInterface $httpClient,
+        private CacheInterface $cache
+    )
+    {
+    }
+
     public function findAll(): array
     {
         //checks to see if we have mixes_data in teh cache. if we do it will return that righ away
         //if not, it will make the http request and set mixes_data to the response array
-        return $cache->get('mixes_data', function(CacheItemInterface $cacheItem) use ($httpClient){
+        return $this->cache->get('mixes_data', function(CacheItemInterface $cacheItem){
             $cacheItem->expiresAfter(5);
-            $response = $httpClient->request('GET', 'https://raw.githubusercontent.com/SymfonyCasts/vinyl-mixes/main/mixes.json');
+            $response = $this->httpClient->request('GET', 'https://raw.githubusercontent.com/SymfonyCasts/vinyl-mixes/main/mixes.json');
             return $response->toArray();
         });
     }
