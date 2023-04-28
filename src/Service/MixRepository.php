865ab8699273e37b523f9ec66a424621e975b069
pheldu95 <phelps.duncan@gmail.com>
2023-04-28 15:39:28 -0500

    used php 8 attribute to autwire a parameter in yet another way...

--- src/Service/MixRepository.php
+++ src/Service/MixRepository.php
@@ -5,12 +5,15 @@ namespace App\Service;
 use Psr\Cache\CacheItemInterface;
 use Symfony\Contracts\Cache\CacheInterface;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
 
 class MixRepository
 {
     public function __construct(
         private HttpClientInterface $httpClient,
         private CacheInterface $cache,
+//        another way to set $isDebug to the kernel.debug parameter
+        #[Autowire('%kernel.debug%')]
         private bool $isDebug
     )
     {
