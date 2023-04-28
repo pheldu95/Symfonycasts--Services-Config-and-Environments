4eec25f675a9eaf0f1e7528182eb33f81d7d874c
pheldu95 <phelps.duncan@gmail.com>
2023-04-28 15:07:51 -0500

    chapter 14. made kernel.debug parameter autowirable by adding config to the services.yaml file

--- src/Service/MixRepository.php
+++ src/Service/MixRepository.php
@@ -10,7 +10,8 @@ class MixRepository
 {
     public function __construct(
         private HttpClientInterface $httpClient,
-        private CacheInterface $cache
+        private CacheInterface $cache,
+        private bool $isDebug
     )
     {
     }
@@ -20,7 +21,8 @@ class MixRepository
         //checks to see if we have mixes_data in teh cache. if we do it will return that righ away
         //if not, it will make the http request and set mixes_data to the response array
         return $this->cache->get('mixes_data', function(CacheItemInterface $cacheItem){
-            $cacheItem->expiresAfter(5);
+            //if we are in debug mode, cache for 5 seconds. else cache for 60
+            $cacheItem->expiresAfter($this->isDebug ? 5 : 60);
             $response = $this->httpClient->request('GET', 'https://raw.githubusercontent.com/SymfonyCasts/vinyl-mixes/main/mixes.json');
             return $response->toArray();
         });
