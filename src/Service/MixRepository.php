bb5ca8eee913d2a44e44bd6e0cad8de86867bf82
pheldu95 <phelps.duncan@gmail.com>
2023-05-01 16:17:09 -0500

    chapter 18. learned how to autowire a service that isn't normally autowirable

--- src/Service/MixRepository.php
+++ src/Service/MixRepository.php
@@ -3,6 +3,9 @@
 namespace App\Service;
 
 use Psr\Cache\CacheItemInterface;
+use Symfony\Bridge\Twig\Command\DebugCommand;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Output\BufferedOutput;
 use Symfony\Contracts\Cache\CacheInterface;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 use Symfony\Component\DependencyInjection\Attribute\Autowire;
@@ -14,13 +17,23 @@ class MixRepository
         private CacheInterface $cache,
 //        another way to set $isDebug to the kernel.debug parameter
         #[Autowire('%kernel.debug%')]
-        private bool $isDebug
+        private bool $isDebug,
+        //since debugcommand isn't autowirable, we use service: 'twig.command.debug' to point to the service
+        #[Autowire (service: 'twig.command.debug')]
+        private DebugCommand $twigDebugCommand
     )
     {
     }
 
     public function findAll(): array
     {
+        //executing a console log
+        /*
+        $output = new BufferedOutput();
+        $this->twigDebugCommand->run(new ArrayInput([]), $output);
+        dd($output);
+        */
+
         //checks to see if we have mixes_data in teh cache. if we do it will return that righ away
         //if not, it will make the http request and set mixes_data to the response array
         return $this->cache->get('mixes_data', function(CacheItemInterface $cacheItem){
